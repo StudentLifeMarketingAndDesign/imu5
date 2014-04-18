37032b91c7f6e034c0073b9ea826bbb4cb61d575
Dustin Quam <quamsta@gmail.com>
2014-04-18 11:25:04 -0500

    updated config

--- mysite/_config.php
+++ mysite/_config.php
@@ -3,26 +3,12 @@
 global $project;
 $project = 'mysite';
 
-global $databaseConfig;
-$databaseConfig = array(
-	"type" => 'MySQLDatabase',
-	"server" => 'localhost',
-	"username" => 'root',
-	"password" => 'omega',
-	"database" => 'imu5',
-	"path" => '',
-);
+global $database;
+$database = 'imu5';
+ 
+// Use _ss_environment.php file for configuration
+require_once("conf/ConfigureFromEnv.php");
 
-MySQLDatabase::set_connection_charset('utf8');
-
-// Set the current theme. More themes can be downloaded from
-// http://www.silverstripe.org/themes/
-SSViewer::set_theme('simple');
 
 // Set the site locale
 i18n::set_locale('en_US');
-FulltextSearchable::enable();
-// Enable nested URLs for this site (e.g. page/sub-page/)
-if (class_exists('SiteTree')) SiteTree::enable_nested_urls();
-
-Director::set_environment_type("dev");

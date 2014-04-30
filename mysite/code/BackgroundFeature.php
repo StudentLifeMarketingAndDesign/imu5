97ed6fb77b6248490ecaed3dfa885be2f2185d06
Dustin Quam <quamsta@gmail.com>
2014-04-30 13:53:54 -0500

    added random homepage feature extension and dataobject

---
+++ mysite/code/BackgroundFeature.php
@@ -0,0 +1,31 @@
+<?php 
+
+	class BackgroundFeature extends DataObject {
+		
+		private static $db = array(
+			'Tagline' => 'Text'
+		);
+		
+		private static $has_one = array (
+			'BackgroundImage' => 'Image',
+			'HomePage' => 'HomePage'
+		);
+
+		private static $plural_name = "Background Features";
+		
+		private static $summary_fields = array (
+			"Tagline",
+			"Thumbnail"
+		);
+	  function getThumbnail() {
+	      return $this->BackgroundImage()->CMSThumbnail();
+	    }		
+		/*function getCMSFields() { 
+			$fields = new FieldList(); 
+			$fields->push( new TextField( 'Title', 'Title' ));
+
+			return $fields; 
+		}*/
+		
+		
+	}

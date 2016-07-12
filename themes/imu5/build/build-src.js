900a918d768092baa8687d1a9bbd8738bb8e2950
Dustin Quam <dustinquam@me.com>
2016-07-12 16:15:37 -0500

    update modules

--- themes/imu5/build/build-src.js
+++ themes/imu5/build/build-src.js
@@ -7045,14 +7045,6 @@ $(document).ready(function() {
     $('.directory-toggle').click(function() {
         $(this).toggleClass("active");
         $('.division-directory').toggleClass("active");
-
-        var state = $(this).attr('aria-expanded') == 'false' ? true : false;
-        $(this).attr('aria-expanded', state);
-
-        var stateHidden = $('#collapsible-0').attr('aria-hidden') == 'false' ? true : false;
-        $('#collapsible-0').attr('aria-hidden', stateHidden);
-
-
         return false;
     });
 
@@ -7061,18 +7053,86 @@ $(document).ready(function() {
     $('.search-toggle').click(function() {
         $(this).toggleClass('active');
         $('.division-search').slideToggle();
-
-        var state = $(this).attr('aria-expanded') == 'false' ? true : false;
-        $(this).attr('aria-expanded', state);
-
-        var stateHidden = $('#collapsible-1').attr('aria-hidden') == 'false' ? true : false;
-        $('#collapsible-1').attr('aria-hidden', stateHidden);
-        
         return false;
     });
 
 
+    // For small screens - show the directory
+    $('.division-menu').on('click', '.has-subnav a', function() {
+        $(this).next().slideToggle('slow');
+        $(this).toggleClass('active');
+
+    });
+
 });
+$(document).ready(function() {
+  var switched = false;
+  var updateTables = function() {
+    if (($(window).width() < 767) && !switched ){
+      switched = true;
+      $("table").each(function(i, element) {
+        splitTable($(element));
+      });
+      return true;
+    }
+    else if (switched && ($(window).width() > 767)) {
+      switched = false;
+      $("table").each(function(i, element) {
+        unsplitTable($(element));
+      });
+    }
+  };
+   
+  $(window).load(updateTables);
+  $(window).on("redraw",function(){switched=false;updateTables();}); // An event to listen for
+  $(window).on("resize", updateTables);
+   
+	
+	function splitTable(original)
+	{
+		original.wrap("<div class='table-wrapper' />");
+		
+		var copy = original.clone();
+		copy.find("td:not(:first-child), th:not(:first-child)").css("display", "none");
+		copy.removeClass("responsive");
+		
+		original.closest(".table-wrapper").append(copy);
+		copy.wrap("<div class='pinned' />");
+		original.wrap("<div class='scrollable' />");
+
+    setCellHeights(original, copy);
+	}
+	
+	function unsplitTable(original) {
+    original.closest(".table-wrapper").find(".pinned").remove();
+    original.unwrap();
+    original.unwrap();
+	}
+
+  function setCellHeights(original, copy) {
+    var tr = original.find('tr'),
+        tr_copy = copy.find('tr'),
+        heights = [];
+
+    tr.each(function (index) {
+      var self = $(this),
+          tx = self.find('th, td');
+
+      tx.each(function () {
+        var height = $(this).outerHeight(true);
+        heights[index] = heights[index] || 0;
+        if (height > heights[index]) heights[index] = height;
+      });
+
+    });
+
+    tr_copy.each(function (index) {
+      $(this).height(heights[index]);
+    });
+  }
+
+});
+
 $(function() {
 
 	if ($('.flexslider').length){
